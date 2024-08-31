52cda45eaa21d8b10e50712041fcd1a65f5cf152
john <jinyngg@gmail.com>
2024-08-31 20:20:42 +0900

    init : project init

---
+++ src/main/java/com/zb/fresh_api/FreshApiApplication.java
@@ -0,0 +1,13 @@
+package com.zb.fresh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshApiApplication.class, args);
+	}
+
+}
