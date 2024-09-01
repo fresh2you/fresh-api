bb815f9e38cf1701502ccd3a93308d99280e386b
john <jinyngg@gmail.com>
2024-09-01 16:59:45 +0900

    feat : p6spy 설정 추가 [#1]

---
+++ src/main/java/com/zb/fresh_api/common/constants/FileConstants.java
@@ -0,0 +1,11 @@
+package com.zb.fresh_api.common.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FileConstants {
+
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+
+}
