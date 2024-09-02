5f1d3f01745e26eca090c762656778e649251a33
Leegeonmin <kmlee97s@gmail.com>
2024-09-03 04:58:14 +0900

    feat : 에러코드에 닉네임 관련 에러 추가 [#2]
    1. 이미 사용 중인 닉네임일 때
    2. 입력한 닉네임이 공백일 때

--- src/main/java/com/zb/fresh_api/api/common/exception/ErrorCode.java
+++ src/main/java/com/zb/fresh_api/api/common/exception/ErrorCode.java
@@ -7,6 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    
+    // 닉네임 관련
+    NICKNAME_ALREADY_IN_USE("이미 사용중인 닉네임입니다", HttpStatus.CONFLICT),
+    PARAM_NICKNAME_NOT_VALID("입력한 닉네임이 잘못되었습니다", HttpStatus.BAD_REQUEST),
+    
+    // 약관 관련
     TERMS_NOT_FOUND("약관 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND);
 
     private final String description;
