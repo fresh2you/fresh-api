8b7b977ca32ae85342ce90dd22557aebc212a791
Leegeonmin <kmlee97s@gmail.com>
2024-09-02 20:27:09 +0900

    feat : 에러 핸들링을 위한 CustomException 추가 [#2]

---
+++ src/main/java/com/zb/fresh_api/api/common/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.zb.fresh_api.api.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends  RuntimeException{
+    private final ErrorCode errorCode;
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
