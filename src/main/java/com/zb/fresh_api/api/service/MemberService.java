7d626fa243bf6c0af3f956139344d3111f991d6b
Leegeonmin <kmlee97s@gmail.com>
2024-09-03 04:59:18 +0900

    feat : 닉네임 중복 검사 로직 수정 [#2]
    1. 닉네임 중복이 있을 시 boolean(false)을 반환하던 것에서 에러를 발생하는 것으로 변경

--- src/main/java/com/zb/fresh_api/api/service/MemberService.java
+++ src/main/java/com/zb/fresh_api/api/service/MemberService.java
@@ -1,14 +1,21 @@
 package com.zb.fresh_api.api.service;
 
+import com.zb.fresh_api.api.common.exception.CustomException;
+import com.zb.fresh_api.api.common.exception.ErrorCode;
 import com.zb.fresh_api.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public boolean isNicknameAvailable(String nickname) {
-        return !memberRepository.existsByNicknameIgnoreCase(nickname);
+    public void nickNameValidate(String nickname) {
+        boolean existsByNicknameIgnoreCase = memberRepository.existsByNicknameIgnoreCase(nickname);
+        if(existsByNicknameIgnoreCase){
+            throw new CustomException(ErrorCode.NICKNAME_ALREADY_IN_USE);
+        }
     }
 }
