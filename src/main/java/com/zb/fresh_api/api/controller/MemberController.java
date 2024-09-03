97e84c0894ccb46b762add5f0084f754978f2717
Leegeonmin <kmlee97s@gmail.com>
2024-09-03 22:00:52 +0900

    feat : 회원가입 API 추가 및 ResponseDto 추가 [#2]

--- src/main/java/com/zb/fresh_api/api/controller/MemberController.java
+++ src/main/java/com/zb/fresh_api/api/controller/MemberController.java
@@ -4,11 +4,15 @@ import com.zb.fresh_api.api.common.GlobalResponse;
 import com.zb.fresh_api.api.common.ResponseCode;
 import com.zb.fresh_api.api.common.exception.CustomException;
 import com.zb.fresh_api.api.common.exception.ErrorCode;
+import com.zb.fresh_api.api.dto.SignUpRequest;
 import com.zb.fresh_api.api.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +50,7 @@ public class MemberController {
         description = "이메일로 회원가입한 사용자 중 중복된 이메일이 있는지 검사합니다"
     )
     @GetMapping("/check-email")
-    public GlobalResponse<?> checkEmailAvailability(
+    public GlobalResponse checkEmailAvailability(
         @RequestParam String email
     ) {
         if (email == null || email.isEmpty()) {
@@ -57,6 +61,16 @@ public class MemberController {
         return GlobalResponse.builder()
             .success(true)
             .message("이메일 중복 확인 성공(중복X)")
+            .code(ResponseCode.SUCCESS.getCode())
+            .build();
+    }
+
+    @PostMapping("/signup")
+    public GlobalResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        memberService.signUp(request.email(), request.password(), request.nickname());
+        return GlobalResponse.builder()
+            .success(true)
+            .message("회원 가입 성공")
             .code(ResponseCode.SUCCESS.getCode())
             .build();
     }
