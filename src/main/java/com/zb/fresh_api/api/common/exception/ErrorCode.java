11b624ca18248e050bc36b985ea992965be8e8f3
Leegeonmin <kmlee97s@gmail.com>
2024-09-03 18:41:31 +0900

    feat : 이메일 중복 검사 로직 추가 및 에러코드 추가 [#2]

--- src/main/java/com/zb/fresh_api/api/common/exception/ErrorCode.java
+++ src/main/java/com/zb/fresh_api/api/common/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     
-    // 닉네임 관련
+    // 회원가입 관련
     NICKNAME_ALREADY_IN_USE("이미 사용중인 닉네임입니다", HttpStatus.CONFLICT),
+    EMAIL_ALREADY_IN_USE("이미 사용중인 이메일입니다", HttpStatus.CONFLICT),
+    PARAM_EMAIL_NOT_VALID("입력한 이메일이 잘못되었습니다", HttpStatus.BAD_REQUEST),
     PARAM_NICKNAME_NOT_VALID("입력한 닉네임이 잘못되었습니다", HttpStatus.BAD_REQUEST),
     
     // 약관 관련
--- src/main/java/com/zb/fresh_api/api/service/MemberService.java
+++ src/main/java/com/zb/fresh_api/api/service/MemberService.java
@@ -2,6 +2,7 @@ package com.zb.fresh_api.api.service;
 
 import com.zb.fresh_api.api.common.exception.CustomException;
 import com.zb.fresh_api.api.common.exception.ErrorCode;
+import com.zb.fresh_api.domain.enums.member.Provider;
 import com.zb.fresh_api.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,13 @@ public class MemberService {
         boolean existsByNicknameIgnoreCase = memberRepository.existsByNicknameIgnoreCase(nickname);
         if(existsByNicknameIgnoreCase){
             throw new CustomException(ErrorCode.NICKNAME_ALREADY_IN_USE);
+        }
+    }
+
+    public void emailValidate(String email) {
+        boolean existsByEmailAndProvider = memberRepository.existsByEmailAndProvider(email, Provider.EMAIL);
+        if(existsByEmailAndProvider){
+            throw new CustomException(ErrorCode.EMAIL_ALREADY_IN_USE);
         }
     }
 }
