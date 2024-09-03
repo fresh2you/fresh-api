21a40b31a102ad237a08afe560e1a0b0e39f50f1
Leegeonmin <kmlee97s@gmail.com>
2024-09-03 21:59:24 +0900

    feat : 회원가입 로직 추가 [#2]

--- src/main/java/com/zb/fresh_api/api/service/MemberService.java
+++ src/main/java/com/zb/fresh_api/api/service/MemberService.java
@@ -2,9 +2,13 @@ package com.zb.fresh_api.api.service;
 
 import com.zb.fresh_api.api.common.exception.CustomException;
 import com.zb.fresh_api.api.common.exception.ErrorCode;
+import com.zb.fresh_api.domain.entity.member.Member;
+import com.zb.fresh_api.domain.enums.member.MemberRole;
+import com.zb.fresh_api.domain.enums.member.MemberStatus;
 import com.zb.fresh_api.domain.enums.member.Provider;
 import com.zb.fresh_api.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
+    private final PasswordEncoder passwordEncoder;
     public void nickNameValidate(String nickname) {
         boolean existsByNicknameIgnoreCase = memberRepository.existsByNicknameIgnoreCase(nickname);
         if(existsByNicknameIgnoreCase){
@@ -25,5 +29,20 @@ public class MemberService {
         if(existsByEmailAndProvider){
             throw new CustomException(ErrorCode.EMAIL_ALREADY_IN_USE);
         }
+    }
+    public void signUp(String email, String password, String nickName) {
+        this.nickNameValidate(nickName);
+        this.emailValidate(email);
+        memberRepository.save(
+            Member.builder()
+                .nickname(nickName)
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .provider(Provider.EMAIL)
+                .role(MemberRole.ROLE_USER)
+                .status(MemberStatus.ACTIVE)
+                .build()
+        );
+
     }
 }
