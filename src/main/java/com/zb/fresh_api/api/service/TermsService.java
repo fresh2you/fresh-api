861944d31a501f754bfc6e52887207749ddb2399
Leegeonmin <kmlee97s@gmail.com>
2024-09-08 19:04:58 +0900

    refactor : TermsService 의존성 변경 [#2]
    1. TermsJpaRepository -> TermsReader

--- src/main/java/com/zb/fresh_api/api/service/TermsService.java
+++ src/main/java/com/zb/fresh_api/api/service/TermsService.java
@@ -1,10 +1,10 @@
 package com.zb.fresh_api.api.service;
 
+import com.zb.fresh_api.api.dto.TermsDto;
 import com.zb.fresh_api.common.exception.CustomException;
 import com.zb.fresh_api.common.exception.ResponseCode;
-import com.zb.fresh_api.api.dto.TermsDto;
 import com.zb.fresh_api.domain.entity.terms.Terms;
-import com.zb.fresh_api.domain.repository.jpa.TermsRepository;
+import com.zb.fresh_api.domain.repository.reader.TermsReader;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class TermsService {
 
-    private final TermsRepository termsRepository;
+    private final TermsReader termsReader;
 
     /**
      * 약관 목록 조회 로직
      * @return 목록ID, 약관명, 필수여부
      */
     public List<TermsDto> getTerms() {
-        List<Terms> terms = termsRepository.findAll();
+        List<Terms> terms = termsReader.findAll();
 
         return terms.stream().map(
             TermsDto::fromEntity
@@ -30,7 +30,7 @@ public class TermsService {
     }
 
     public TermsDto getTerm(final Long termsId) {
-        Terms terms = termsRepository.findById(termsId).orElseThrow(
+        Terms terms = termsReader.findById(termsId).orElseThrow(
             () -> new CustomException(ResponseCode.TERMS_NOT_FOUND)
         );
         return TermsDto.fromEntity(terms);
