74e963cc8b4b94c591c997a4fa05e0fb439121f1
Leegeonmin <kmlee97s@gmail.com>
2024-09-09 01:05:44 +0900

    feat : redisConstants의 email,sms prefix 삭제 [#23]
    1. 이미 CertificationType으로 분리해서 있을 필요 없음

--- src/main/java/com/zb/fresh_api/common/constants/RedisConstants.java
+++ src/main/java/com/zb/fresh_api/common/constants/RedisConstants.java
@@ -5,8 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RedisConstants {
-    public static final String PHONE_PREFIX = "sms:";
-    public static final String EMAIL_PREFIX = "email:";
     public static final String ATTEMPT_PREFIX = "attempt:";
     public static final int CODE_EXPIRATION_TIME = 3 * 60;
     public static final int ATTEMPT_EXPIRATION_TIME = 24 * 60 * 60;
