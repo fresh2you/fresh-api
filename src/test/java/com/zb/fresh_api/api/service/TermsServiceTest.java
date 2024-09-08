02e625bc87b4c1ebf00530527df5d4da95fac6fc
Leegeonmin <kmlee97s@gmail.com>
2024-09-08 19:05:16 +0900

    test : TermsServiceTest 코드 변경 [#2]

--- src/test/java/com/zb/fresh_api/api/service/TermsServiceTest.java
+++ src/test/java/com/zb/fresh_api/api/service/TermsServiceTest.java
@@ -10,11 +10,10 @@ import com.zb.fresh_api.api.dto.TermsDto;
 import com.zb.fresh_api.common.exception.CustomException;
 import com.zb.fresh_api.common.exception.ResponseCode;
 import com.zb.fresh_api.domain.entity.terms.Terms;
-import com.zb.fresh_api.domain.repository.jpa.TermsRepository;
+import com.zb.fresh_api.domain.repository.reader.TermsReader;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,30 +24,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class TermsServiceTest {
     @Mock
-    private TermsRepository termsRepository;
+    private TermsReader termsReader;
 
     @InjectMocks
     private TermsService termsService;
-
-
-    private Terms terms;
-    private TermsDto termsDto;
-
-    @BeforeEach
-    void setUp() {
-        terms = Terms.builder()
+    @Test
+    @DisplayName("약관 목록 조회 성공")
+    void getTerms_success(){
+        // given
+        Terms terms = Terms.builder()
             .id(1L)
             .title("약관 타이틀")
             .isRequired(true)
             .content("약관 내용")
             .build();
-        termsDto = TermsDto.fromEntity(terms);
-    }
-    @Test
-    @DisplayName("약관 목록 조회 성공")
-    void getTerms_success(){
-        // given
-        when(termsRepository.findAll()).thenReturn(Collections.singletonList(terms));
+        TermsDto termsDto = TermsDto.fromEntity(terms);
+        when(termsReader.findAll()).thenReturn(Collections.singletonList(terms));
         // when
         List<TermsDto> result = termsService.getTerms();
         // then
@@ -60,19 +51,26 @@ class TermsServiceTest {
     @DisplayName("약관 상세 조회 성공")
     void getTerm_success(){
         // given
-        when(termsRepository.findById(anyLong())).thenReturn(Optional.of(terms));
+        Terms terms = Terms.builder()
+            .id(1L)
+            .title("약관 타이틀")
+            .isRequired(true)
+            .content("약관 내용")
+            .build();
+        TermsDto dto = TermsDto.fromEntity(terms);
+        when(termsReader.findById(anyLong())).thenReturn(Optional.of(terms));
         // when
         TermsDto result = termsService.getTerm(1L);
         // then
         assertNotNull(result);
-        assertEquals(termsDto, result);
+        assertEquals(dto, result);
     }
 
     @Test
     @DisplayName("약관 상세 조회 실패(약관이 없음)")
     void getTerm_fail_terms_not_found(){
         // given
-        when(termsRepository.findById(anyLong())).thenReturn(Optional.empty());
+        when(termsReader.findById(anyLong())).thenReturn(Optional.empty());
         // then
         CustomException customException = assertThrows(CustomException.class,
             () -> termsService.getTerm(1L));
