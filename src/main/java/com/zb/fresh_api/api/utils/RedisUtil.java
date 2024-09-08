770420e1b1e701efaa98bb9608ebd879b6212879
Leegeonmin <kmlee97s@gmail.com>
2024-09-09 01:04:22 +0900

    feat : RedisUtil 메서드 수정 [#23]
    1. 모든 메서드가 CertificationType을 입력받도록 수정

--- src/main/java/com/zb/fresh_api/api/utils/RedisUtil.java
+++ src/main/java/com/zb/fresh_api/api/utils/RedisUtil.java
@@ -1,5 +1,6 @@
 package com.zb.fresh_api.api.utils;
 
+import com.zb.fresh_api.api.enums.CertificationType;
 import com.zb.fresh_api.common.constants.RedisConstants;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -14,28 +15,28 @@ public class RedisUtil {
     private final StringRedisTemplate redisTemplate;
 
 
-
-    // 문자 인증의 인증 코드 저장 메서드
-    public void saveCertificationCode(String phone, String certificationNumber) {
-        redisTemplate.opsForValue()
-            .set(RedisConstants.PHONE_PREFIX + phone, certificationNumber, Duration.ofSeconds(RedisConstants.CODE_EXPIRATION_TIME));
+    // 인증 코드 저장 메서드
+    public void saveCertificationCode(String phone, String certificationNumber,
+        CertificationType certificationType) {
+        redisTemplate.opsForValue().set(certificationType.getPrefix() + phone, certificationNumber,
+            Duration.ofSeconds(RedisConstants.CODE_EXPIRATION_TIME));
     }
 
-    // 문자 인증 코드 찾는 메서드
-    public String findSmsCertification(String phone) {
-        return redisTemplate.opsForValue().get(RedisConstants.PHONE_PREFIX + phone);
+    //  인증 코드 찾는 메서드
+    public String findSmsCertification(String phone, CertificationType certificationType) {
+        return redisTemplate.opsForValue().get(certificationType.getPrefix() + phone);
     }
 
-    // 문자 인증 코드 제거 메서드
-    public void removeSmsCertification(String phone) {
-        redisTemplate.delete(RedisConstants.PHONE_PREFIX + phone);
-        redisTemplate.delete(RedisConstants.PHONE_PREFIX + RedisConstants.ATTEMPT_PREFIX + phone);
+    //  인증 코드 제거 메서드
+    public void removeSmsCertification(String phone, CertificationType certificationType) {
+        redisTemplate.delete(certificationType.getPrefix() + phone);
+        redisTemplate.delete(certificationType.getPrefix() + RedisConstants.ATTEMPT_PREFIX + phone);
     }
 
 
     // 인증 횟수 초과 확인 메서드
-    public boolean hasExceededAttemptLimit(String phone) {
-        String key = RedisConstants.PHONE_PREFIX + RedisConstants.ATTEMPT_PREFIX + phone;
+    public boolean hasExceededAttemptLimit(String to, CertificationType type) {
+        String key = type.getPrefix() + RedisConstants.ATTEMPT_PREFIX + to;
         String attemptsData = redisTemplate.opsForValue().get(key);
         if (attemptsData != null) {
             String[] parts = attemptsData.split(":");
@@ -48,9 +49,9 @@ public class RedisUtil {
         return false;
     }
 
-    // 문자 인증 횟수 기록 메서드
-    public void recordAttempt(String phone) {
-        String key = RedisConstants.PHONE_PREFIX + RedisConstants.ATTEMPT_PREFIX + phone;
+    // 인증 횟수 기록 메서드
+    public void recordAttempt(String phone,CertificationType certificationType) {
+        String key = certificationType.getPrefix() + RedisConstants.ATTEMPT_PREFIX + phone;
         String attemptsData = redisTemplate.opsForValue().get(key);
         int attempts = 0;
         LocalDate date = LocalDate.now();
@@ -66,7 +67,7 @@ public class RedisUtil {
         } else {
             attempts = 1;
         }
-        redisTemplate.opsForValue()
-            .set(key, attempts + ":" + date, Duration.ofSeconds(RedisConstants.ATTEMPT_EXPIRATION_TIME));
+        redisTemplate.opsForValue().set(key, attempts + ":" + date,
+            Duration.ofSeconds(RedisConstants.ATTEMPT_EXPIRATION_TIME));
     }
 }
