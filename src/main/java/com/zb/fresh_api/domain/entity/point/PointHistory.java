e3279fd9ec5715f4c75b285336e24c4dbda47eea
Leegeonmin <kmlee97s@gmail.com>
2024-09-10 03:37:18 +0900

    feat : point, pointHistory 엔티티 추가 [#22]

---
+++ src/main/java/com/zb/fresh_api/domain/entity/point/PointHistory.java
@@ -0,0 +1,64 @@
+package com.zb.fresh_api.domain.entity.point;
+
+import com.zb.fresh_api.domain.entity.base.BaseTimeEntity;
+import com.zb.fresh_api.domain.entity.member.Member;
+import com.zb.fresh_api.domain.enums.point.PointTransactionType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "point_history"
+)
+public class PointHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED comment '고유 번호'")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '회원 고유 번호'")
+    private Member member;
+
+    //    Todo 오더 추가 후 작업
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "order_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '오더 고유 번호'")
+//    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "point_transaction_type", nullable = false, columnDefinition = "varchar(20) comment '거래유형'")
+    private PointTransactionType pointTransactionType;
+
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2, columnDefinition = "DECIMAL(10,2) comment '거래 금액'")
+    private BigDecimal amount;
+
+    @Column(name = "balanceBefore", nullable = false, precision = 10, scale = 2, columnDefinition = "DECIMAL(10,2) comment '거래 전 잔액'")
+    private BigDecimal balanceBefore;
+
+    @Column(name = "balanceAfter", nullable = false, precision = 10, scale = 2, columnDefinition = "DECIMAL(10,2) comment '거래 후 잔액'")
+    private BigDecimal balanceAfter;
+
+    @Column(name = "description", nullable = true, columnDefinition = "varchar(255) comment '거래 설명'")
+    private String description;
+
+}
