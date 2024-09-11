525214a5a7254ba7003295045697da7284a0e9ef
Leegeonmin <kmlee97s@gmail.com>
2024-09-11 21:37:24 +0900

    feat : 상품 엔티티 추가 [#32]

---
+++ src/main/java/com/zb/fresh_api/domain/entity/product/Product.java
@@ -0,0 +1,61 @@
+package com.zb.fresh_api.domain.entity.product;
+
+import com.zb.fresh_api.domain.entity.base.BaseTimeEntity;
+import com.zb.fresh_api.domain.entity.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "product"
+)
+public class Product extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED comment '고유 번호'")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '회원 고유 번호'")
+    private Member member;
+
+//    @OneToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "category_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '카테고리 고유 번호'")
+//    private Category category;
+
+    @JoinColumn(name = "name", nullable = false, columnDefinition = "varchar(20)  comment '상품명'")
+    private String name;
+
+    @JoinColumn(name = "description", nullable = false, columnDefinition = "text  comment '상품설명'")
+    private String description;
+
+    @Column(name = "quantity", nullable = false, columnDefinition = "INT UNSIGNED comment '재고'")
+    private int quantity;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2, columnDefinition = "DECIMAL(10,2) comment '가격'")
+    private BigDecimal price;
+
+    @LastModifiedDate
+    @Column(name = "deleted_at", columnDefinition = "datetime comment '삭제일'")
+    private LocalDateTime deleted_at;
+
+}
