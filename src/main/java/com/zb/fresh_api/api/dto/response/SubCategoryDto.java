c0f3b2dd53efe7e704b601369ffd7889caa8b710
Leegeonmin <kmlee97s@gmail.com>
2024-09-12 05:59:56 +0900

    chore : chore [#32]

--- src/main/java/com/zb/fresh_api/api/dto/response/SubCategoryDto.java
+++ src/main/java/com/zb/fresh_api/api/dto/response/SubCategoryDto.java
@@ -1,9 +1,3 @@
 package com.zb.fresh_api.api.dto.response;
 
-import com.zb.fresh_api.domain.entity.category.Category;
-
-public record SubCategoryDto (Long categoryId, String categoryName){
-    public static SubCategoryDto fromCategory(Category category) {
-        return new SubCategoryDto(category.getId(), category.getName());
-    }
-}
+public record SubCategoryDto(Long categoryId, String categoryName) {}
