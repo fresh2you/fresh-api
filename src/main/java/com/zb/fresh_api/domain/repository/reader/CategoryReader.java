12c26b6927b16b801050a647aa129162519b3b15
Leegeonmin <kmlee97s@gmail.com>
2024-09-12 05:21:46 +0900

    feat : 카테고리 엔티티, 레포지토리, Reader추가  [#32]
    1. 모든 카테고리 조회 로직도 추가

---
+++ src/main/java/com/zb/fresh_api/domain/repository/reader/CategoryReader.java
@@ -0,0 +1,19 @@
+package com.zb.fresh_api.domain.repository.reader;
+
+import com.zb.fresh_api.domain.annotation.Reader;
+import com.zb.fresh_api.domain.entity.category.Category;
+import com.zb.fresh_api.domain.repository.jpa.CategoryJpaRepository;
+import com.zb.fresh_api.domain.repository.query.CategoryQueryRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@Reader
+@RequiredArgsConstructor
+public class CategoryReader {
+    private final CategoryJpaRepository categoryJpaRepository;
+    private final CategoryQueryRepository categoryQueryRepository;
+
+    public List<Category> findAll(){
+        return categoryQueryRepository.findAll();
+    }
+}
