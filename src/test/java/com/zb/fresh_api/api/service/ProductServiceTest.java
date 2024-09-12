2fbc6848140637a4d1d81250936de494f1e9b55f
Leegeonmin <kmlee97s@gmail.com>
2024-09-13 02:32:49 +0900

    test : 제품 등록 이미지 추가로 테스트 코드 수정 [#32]

--- src/test/java/com/zb/fresh_api/api/service/ProductServiceTest.java
+++ src/test/java/com/zb/fresh_api/api/service/ProductServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import com.zb.fresh_api.api.dto.request.AddProductRequest;
@@ -23,9 +24,10 @@ import java.math.BigDecimal;
 import java.util.Optional;
 import net.jqwik.api.Arbitraries;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.web.multipart.MultipartFile;
 
 @DisplayName("제품 관련 비즈니스 테스트")
 class ProductServiceTest extends ServiceTest {
@@ -40,8 +42,7 @@ class ProductServiceTest extends ServiceTest {
     private ProductService productService;
 
     @DisplayName("제품 등록 성공")
-    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
-
+    @Test
     void addProduct_success() {
         // given
         Long categoryId = Arbitraries.longs().greaterOrEqual(1L).sample();
@@ -70,6 +71,7 @@ class ProductServiceTest extends ServiceTest {
             .set("category", category)
             .set("name" , addProductRequest.name())
             .sample();
+        MultipartFile multipartFile = mock(MultipartFile.class);
 
         doReturn(Optional.of(category)).when(categoryReader)
             .findById(addProductRequest.categoryId());
@@ -82,7 +84,7 @@ class ProductServiceTest extends ServiceTest {
         );
         // When
         AddProductResponse response = productService.addProduct(addProductRequest,
-            member);
+            member, multipartFile);
 
         // then
         assertNotNull(response);
@@ -92,8 +94,7 @@ class ProductServiceTest extends ServiceTest {
     }
 
     @DisplayName("제품 등록 실패 - 사용자가 판매자가 아님")
-    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
-
+    @Test
     void addProduct_fail_UNAUTHORIZED() {
         // given
         AddProductRequest addProductRequest = getConstructorMonkey().giveMeBuilder(
@@ -112,16 +113,18 @@ class ProductServiceTest extends ServiceTest {
             .set("isSeller", false)
             .sample();
 
+        MultipartFile multipartFile = mock(MultipartFile.class);
+
         // When
-        CustomException exception = assertThrows(CustomException.class, () -> productService.addProduct(addProductRequest, member));
+        CustomException exception = assertThrows(CustomException.class, () -> productService.addProduct(addProductRequest, member,
+            multipartFile));
 
         // then
         assertEquals(ResponseCode.UNAUTHORIZED_ACCESS_EXCEPTION, exception.getResponseCode());
     }
 
     @DisplayName("제품 등록 실패 - 카테고리 ID가 유효하지 않음")
-//    @Test
-    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+    @Test
     void addProduct_fail_INVALID_CATEGORYID() {
         // given
         AddProductRequest addProductRequest = getConstructorMonkey().giveMeBuilder(
@@ -139,11 +142,13 @@ class ProductServiceTest extends ServiceTest {
                 Arbitraries.strings().withCharRange('a', 'z').ofMinLength(1).ofMaxLength(50))
             .set("isSeller", true)
             .sample();
+        MultipartFile multipartFile = mock(MultipartFile.class);
 
         doReturn(Optional.empty()).when(categoryReader).findById(addProductRequest.categoryId());
 
         // When
-        CustomException exception = assertThrows(CustomException.class, () -> productService.addProduct(addProductRequest, member));
+        CustomException exception = assertThrows(CustomException.class, () -> productService.addProduct(addProductRequest, member,
+            multipartFile));
 
         // then
         assertEquals(ResponseCode.CATEGORY_NOT_VALID, exception.getResponseCode());
