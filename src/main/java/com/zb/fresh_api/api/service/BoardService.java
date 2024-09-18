7dbd49d7313ed395fa708ae2044704775391f304
Leegeonmin <kmlee97s@gmail.com>
2024-09-18 21:04:19 +0900

    feat : 게시판 수정 API 추가 [#32]

--- src/main/java/com/zb/fresh_api/api/service/BoardService.java
+++ src/main/java/com/zb/fresh_api/api/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.zb.fresh_api.api.service;
 
+import com.zb.fresh_api.api.dto.request.UpdateBoardRequest;
 import com.zb.fresh_api.api.dto.response.AddBoardResponse;
+import com.zb.fresh_api.api.dto.response.UpdateBoardResponse;
 import com.zb.fresh_api.common.exception.CustomException;
 import com.zb.fresh_api.common.exception.ResponseCode;
 import com.zb.fresh_api.domain.entity.board.Board;
@@ -38,5 +40,19 @@ public class BoardService {
         Board board = boardWriter.store(Board.create(member, product, title));
 
         return new AddBoardResponse(board.getId(), board.getTitle(), board.getCreatedAt());
+    }
+
+    public UpdateBoardResponse updateBoard(Long memberId, UpdateBoardRequest request, Long boardId) {
+        Member member = memberReader.getById(memberId);
+        Board board = boardReader.getById(boardId);
+        if(!Objects.equals(board.getMember().getId(), memberId)){
+            throw new CustomException(ResponseCode.NOT_PRODUCT_OWNER);
+        }
+
+        board.update(request.title());
+        boardWriter.store(board);
+
+        return new UpdateBoardResponse(board.getId(),board.getTitle(),board.getUpdatedAt());
+
     }
 }
--- src/main/java/com/zb/fresh_api/domain/repository/reader/BoardReader.java
+++ src/main/java/com/zb/fresh_api/domain/repository/reader/BoardReader.java
@@ -1,6 +1,9 @@
 package com.zb.fresh_api.domain.repository.reader;
 
+import com.zb.fresh_api.common.exception.CustomException;
+import com.zb.fresh_api.common.exception.ResponseCode;
 import com.zb.fresh_api.domain.annotation.Reader;
+import com.zb.fresh_api.domain.entity.board.Board;
 import com.zb.fresh_api.domain.repository.jpa.BoardJpaRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +14,10 @@ public class BoardReader {
 
     public boolean isExistBySameProduct(Long productId) {
         return boardJpaRepository.existsByProductId(productId);
+    }
+
+    public Board getById(final Long id) {
+        return boardJpaRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ResponseCode.BOARD_NOT_FOUND));
     }
 }
