ed1aef9a42645798705fb78de2ed165ad907687f
john <jinyngg@gmail.com>
2024-09-20 01:08:18 +0900

    feat : 구매자 정보 Member 정보 추가 [#38]

--- src/main/java/com/zb/fresh_api/domain/entity/member/Customer.java
+++ src/main/java/com/zb/fresh_api/domain/entity/member/Customer.java
@@ -20,6 +20,10 @@ public class Customer {
     @Column(name = "id", columnDefinition = "BIGINT UNSIGNED comment '고유 번호'")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '회원 고유 번호'")
+    private Member member;
+
     @Column(name = "email", nullable = false, columnDefinition = "varchar(255) comment '이메일'")
     private String email;
 
@@ -35,6 +39,7 @@ public class Customer {
 
     public static Customer of(Member member){
         return Customer.builder()
+                .member(member)
                 .email(member.getEmail())
                 .nickname(member.getNickname())
                 .phone(member.getPhone())
