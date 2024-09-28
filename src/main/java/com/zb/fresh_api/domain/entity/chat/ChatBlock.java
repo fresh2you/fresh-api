afdb2977bc748cbc691b80c860095bd65e62e512
LeeHonggyo <ghdry2@gmail.com>
2024-09-28 18:48:54 +0900

    feat : 채팅 엔티티 수정 [#26]

--- src/main/java/com/zb/fresh_api/domain/entity/chat/ChatBlock.java
+++ src/main/java/com/zb/fresh_api/domain/entity/chat/ChatBlock.java
@@ -1,5 +1,6 @@
 package com.zb.fresh_api.domain.entity.chat;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,9 +15,13 @@ public class ChatBlock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false, columnDefinition = "BIGINT UNSIGNED COMMENT 'Primary Key'")
     private Long id;
 
+    @Column(name = "chat_blocker_id", nullable = false, columnDefinition = "BIGINT COMMENT '차단한 사용자 ID'")
     private Long chatBlockerId;
+
+    @Column(name = "chat_blocked_id", nullable = false, columnDefinition = "BIGINT COMMENT '차단당한 사용자 ID'")
     private Long chatBlockedId;
 
     public static ChatBlock create(Long chatBlockerId, Long chatBlockedId) {
